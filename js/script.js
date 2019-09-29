faca62a15c254a0292a9a43db9574a7ae75b1997
nasri samy <nasri.samy.dev@gmail.com>
2019-09-29 19:05:53 +0200

    add a variable for the scorekeeper

---
+++ js/script.js
@@ -0,0 +1,8 @@
+ let player1 = document.querySelector("#player1");
+ let player2 = document.querySelector("#player2");
+ let clear = document.querySelector('#clear');
+ let score1 = document.querySelector('#score1');
+ let score2 = document.querySelector('#score2');
+ let cpt1=0;
+ let cpt2=0;
+ let defaultScore =5;
